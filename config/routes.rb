bbcd5f4f550f894ad64b18132f7e98a32d72874d
Tyler Ewing <tewing10@gmail.com>
2024-06-29 08:56:13 -0500

    feat: add a homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root "home#index"
+
   post "rubygems" => "rubygems#create"
 
   get "up" => "rails/health#show", as: :rails_health_check

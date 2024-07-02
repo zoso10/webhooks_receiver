57cff95e3a0156a6f8131310e49c5ce9cf70c38f
Tyler Ewing <tewing10@gmail.com>
2024-07-02 07:18:21 -0500

    fix: skip CSRF token verification since it's an API

--- app/controllers/rubygems_controller.rb
+++ app/controllers/rubygems_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class RubygemsController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def create
     WebhookRequest
       .create!(source:, body:, headers:, metadata: { name:, version: })
